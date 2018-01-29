8e25544404cb5a349b07669a0d1a565d31f1c13d
mariomijan <mijanmario@gmail.com>
2018-01-29 09:55:53 +0100

    Add files via upload

---
+++ Admin/Admin.Master.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UserLogin;
+
+namespace Svendeproeve.Admin
+{
+    public partial class Admin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                if (!UserLogin.User.IsInRole(UserLogin.Role.Admin))
+                {
+
+
+                    if (IsPostBack)
+                    {
+                        Response.Redirect("/");
+
+                    }
+
+                    if (UserLogin.User.IsInRole(UserLogin.Role.User))
+                    {
+
+                        Response.Redirect("~/Default.aspx");
+
+
+                    }
+                }
+
+                else
+
+                    lblUsername.Text = "Welcome " + CurrentUser.GetUser().Username;
+            }
+        }
+
+        protected void btnLogOff_Click(object sender, EventArgs e)
+        {
+            CurrentUser.LogOff();
+        }
+    }
+}
---
+++ Admin/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Svendeproeve.Admin.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="AdminContent" runat="server">
+    
+    
+    
+
+</asp:Content>
